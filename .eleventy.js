6c8703c653e49f2b7cf90d6eb7fda12e04b66936
Julie Xiong <juliexiong@users.noreply.github.com>
2021-08-18 12:38:27 -0500

    build: add inclusive language linter

--- .eleventy.js
+++ .eleventy.js
@@ -79,6 +79,20 @@ module.exports = function(eleventyConfig) {
     .use(markdownItAnchor, opts)
   );
 
+  eleventyConfig.addLinter("inclusive-language", function(content, inputPath, outputPath) {
+    let words = "simply,obviously,basically,of course,clearly,just,everyone knows,however,easy".split(",");
+
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( inputPath.endsWith(".md") ) {
+      for( let word of words) {
+        let regexp = new RegExp("\\b(" + word + ")\\b", "gi");
+        if(content.match(regexp)) {
+          console.warn(`Inclusive Language Linter (${inputPath}) Found: ${word}`);
+        }
+      }
+    }
+  });
+
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
 
@@ -98,11 +112,16 @@ module.exports = function(eleventyConfig) {
       output: "_site",
       layouts: "_includes/layouts"
     },
+
     eleventyComputed: {
       eleventyNavigation: {
         key: data => data.title,
         parent: data => {
+          return "poop";
           const pathParts = data.inputPath.split("/");
+          if (pathParts.length <= 1) {
+            return null;
+          }
           const parent = pathParts[pathParts.length - 2];
           return parent;
         }
