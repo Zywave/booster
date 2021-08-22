f8a39f70b7582dce08aea9bb85c2a6b9e5ad28ed
Hudson, David <david.hudson@zywave.com>
2021-08-21 22:10:53 -0500

    refactor: tabbing JS tidying

--- src/_includes/assets/scripts/component-tabbing.ts
+++ src/_includes/assets/scripts/component-tabbing.ts
@@ -1,18 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
     const tabsEl = document.querySelector('zui-tabs');
-    
+
     function getTabQueryParam() {
-        const tabVal = window.location.href.split('?tab=')[1].split('#')[0] ?? window.location.href.split('?tab=')[1];
-        return tabVal;
-    }
-    
-    function getTabWithMatchingVal(val: string) {
-        return document.querySelector(`#tab-${val}`);
+        return window.location.href.split('?tab=')[1].split('#')[0]
+            ?? window.location.href.split('?tab=')[1];
     }
 
-    function showApiTabContent() {
-        tabsEl.setAttribute('selected', '0');
-        document.querySelector('#api').classList.remove('is-hidden');
+    function getTabWithMatchingVal(val: string) {
+        return document.querySelector(`#tab-${val}`);
     }
 
     function getIndexPosOfQueryInTabs() {
@@ -25,15 +20,27 @@ window.addEventListener('DOMContentLoaded', function() {
             );
     }
 
+    function hideAllTabContent() {
+        document.querySelectorAll('#content > [id]')
+            .forEach(function(el) {
+                if (el.classList.contains('is-hidden')) return;
+                el.classList.add('is-hidden');
+            }
+        );
+    }
+
+    function showApiTabContent() {
+        tabsEl.setAttribute('selected', '0');
+        document.querySelector('#api')?.classList.remove('is-hidden');
+    }
+
     if (window.location.href.includes('?tab=')) {
+        // if ?tab=* val doesn't match a tab, show and highlight API as selected tab
         if (getIndexPosOfQueryInTabs() === -1) {
             showApiTabContent()
         } else {
             tabsEl.setAttribute('selected', String(getIndexPosOfQueryInTabs()));
-            document.querySelectorAll('#content > [id]').forEach(function(el) {
-                if (el.classList.contains('is-hidden')) return;
-                el.classList.add('is-hidden');
-            })
+            hideAllTabContent();
             document.querySelector(`#${getTabQueryParam()}`)?.classList.remove('is-hidden');
         }
     } else {
