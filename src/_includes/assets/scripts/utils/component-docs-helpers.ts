01e4f7ce47afa25dc7b589be1337b32bd615cce1
Hudson, David <david.hudson@zywave.com>
2021-08-26 17:25:49 -0500

    fix & refactor: hash url, reorg tabs as modules

---
+++ src/_includes/assets/scripts/utils/component-docs-helpers.ts
@@ -0,0 +1,44 @@
+const url = new URL(String(document.location));
+
+export function getQueryParamValue(queryParam: string) {
+    return url.searchParams.get(queryParam);
+}
+
+export function getTabElementById(id: string) {
+    return document.getElementById(`tab-${id}`);
+}
+
+export function getIndexPosOfQueryInTabs(parentElement: Element, queryParam: string) {
+    return Array
+        .from(parentElement?.children)
+        .indexOf(
+            getTabElementById(
+                getQueryParamValue(queryParam)
+            )
+        );
+}
+
+export function hideAllTabContent() {
+    document.querySelectorAll('#content > [id]')
+        .forEach(function(el) {
+            if (el.classList.contains('is-hidden')) return;
+            el.classList.add('is-hidden');
+        }
+    );
+}
+
+export function showApiTabContent(element: Element) {
+    element.setAttribute('selected', '0');
+    document.querySelector('#api')?.classList.remove('is-hidden');
+}
+
+export function getUrlNoHash() {
+    return window.location.href.split("#")[0];
+}
+
+export function createClipboardCopyIconElement(clipboardVal: string) {
+    const el = document.createElement('clipboard-copy-icon');
+    el.setAttribute('icon', 'link');
+    el.setAttribute('clipboard', clipboardVal);
+    return el;
+}
