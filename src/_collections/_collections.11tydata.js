0fc43952f28a1d17b9881842ffc7afcc105837ad
Patrick O'Brien <patrick.obrien@zywave.com>
2021-09-07 12:10:02 -0500

    fix: remove _collections from routes

--- src/_collections/_collections.11tydata.js
+++ src/_collections/_collections.11tydata.js
@@ -1,21 +1,48 @@
-module.exports = {
-    eleventyComputed: {
-      key: data => data.title,
-      parent: data => {
-        if (!data.page?.inputPath?.length) {
-            return undefined;
-        }
-        const pathParts = data.page.inputPath.split("/");
-        const parent = pathParts[pathParts.length - 2];
-        return parent;
-      },
-      system: data => {
-        if (!data.page?.inputPath?.length) {
-          return undefined;
-        }
-        const pathParts = data.page.inputPath.split("/");
-        const system = pathParts[pathParts.length - 3];
-        return system;
-      }
+function parseMetadata(data) {
+  let parent;
+  let system;
+  if (data.page?.inputPath?.length) {
+    const inputPath = data.page.inputPath;
+    const parts = inputPath.split("/_collections/");
+    if (parts.length > 1) {
+      const rightPart = parts[1];
+      const splitOnSlash = rightPart.split("/");
+      system = splitOnSlash[0];
+      parent = splitOnSlash[splitOnSlash.length - 2] ?? system;
     }
-  };
+  }
+
+  return {
+    data,
+    parent,
+    system
+  }
+}
+
+
+module.exports = {
+  eleventyComputed: {
+    key: data => data.title,
+    parent: data => {
+      const metadata = parseMetadata(data);
+      return metadata.parent;
+    },
+    system: data => {
+      const metadata = parseMetadata(data);
+      return metadata.system;
+    },
+    permalink: data => {
+      const metadata = parseMetadata(data);
+      if (!metadata.system) {
+        return undefined;
+      }
+
+      let restOfPath = data.page.inputPath.split(`${metadata.system}/`)[1];
+      if (!restOfPath) {
+        console.log(data.page.inputPath);
+      }
+      restOfPath = restOfPath.replace(/\.[^/.]+$/, "");
+      return `${metadata.system}/${restOfPath}/`;
+    }
+  }
+};
