741cc3bacf8ce4cec779b10635af3dcf6220b98d
Julie Xiong <julie.xiong@zywave.com>
2022-11-17 17:51:45 -0600

    chore: export env var for Heap

--- src/_data/env.js
+++ src/_data/env.js
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
     ALGOLIA_SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
     ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-    SITE_ID: process.env.SITE_ID
+    SITE_ID: process.env.SITE_ID,
+    HEAP_ID: process.env.HEAP_ID
 };
